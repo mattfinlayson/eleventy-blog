bab845d400e66810095ebb233a66c80acd40565a
Matthew Finlayson <mfinlayson@bgrove.com>
2023-03-17 09:41:25 -0700

    Github Actions and CSS

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
 
+    // Folders to copy to output folder
+    eleventyConfig.addPassthroughCopy("css");
 };
 
 function extractExcerpt(article) {
